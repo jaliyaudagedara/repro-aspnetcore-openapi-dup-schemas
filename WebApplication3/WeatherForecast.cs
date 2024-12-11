2afacc664aca0b993ccca5cab2cb018e15472c98
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2024-12-11 23:09:59 +1300

    Remove extra DTO properties for easy debugging.

--- WebApplication3/WeatherForecast.cs
+++ WebApplication3/WeatherForecast.cs
@@ -2,25 +2,15 @@
 
 public class WeatherForecast
 {
-    public int Id { get; set; }
-
-    public DateOnly Date { get; set; }
-
-    public int TemperatureC { get; set; }
-
     public LocationDto Location { get; set; }
 }
 
 public class LocationDto
 {
-    public string Name { get; set; }
-
     public AddressDto Address { get; set; }
 }
 
 public class AddressDto
 {
-    string AddressLine { get; set; }
-
     public LocationDto RelatedLocation { get; set; }
 }
